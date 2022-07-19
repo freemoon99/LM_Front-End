c9f6ae405872409510be8a3ce2cc93a1f1b663f6
syhan7516 <syhan7516@naver.com>
2022-07-20 02:48:38 +0900

    페이지에 따른 Header title data logic 추가

--- leave_message/src/pages/SettingsPage.js
+++ leave_message/src/pages/SettingsPage.js
@@ -1,6 +1,12 @@
+import Header from "../components/Header";
+
 const SettingsPage = () => {
+
+    const pageName = 'setting';
+
     return(
         <>
+            <Header pageName={pageName}/>
             <h2>설정 페이지</h2>
         </>
     );
--- leave_message/src/pages/UpdatePage.js
+++ leave_message/src/pages/UpdatePage.js
@@ -1,6 +1,12 @@
+import Header from "../components/Header";
+
 const UpdatePage = () => {
+
+    const pageName = 'update';
+
     return(
         <>
+            <Header pageName={pageName}/>
             <h2>수정 페이지</h2>
         </>
     );
