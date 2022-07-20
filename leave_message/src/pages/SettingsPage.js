447b79b16053610efd53889115fcc78cf89938ac
syhan7516 <syhan7516@naver.com>
2022-07-20 22:42:40 +0900

    헤더/바디/푸터 설정 페이지에 적용

--- leave_message/src/pages/SettingsPage.js
+++ leave_message/src/pages/SettingsPage.js
@@ -1,14 +1,21 @@
 import Header from "../components/Header";
+import Footer from "../components/Footer";
+import "../styles/pages/SettingsPage.css";
 
 const SettingsPage = () => {
 
     const pageName = 'setting';
 
     return(
-        <>
-            <Header pageName={pageName}/>
-            <h2>설정 페이지</h2>
-        </>
+        <div className="frame-settingsPage">
+            <Header pageName={pageName} className="frame-settingsPage-header"/>
+            
+            <div className="frame-settingsPage-body">
+                <h2>설정 페이지</h2>
+            </div>
+
+            <Footer className="frame-settingsPage-footer"/>
+        </div>
     );
 }
 
