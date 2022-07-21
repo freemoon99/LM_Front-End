2e7ab0ef860981abf59cd9460c11481e079cc7c3
syhan7516 <syhan7516@naver.com>
2022-07-21 13:02:15 +0900

    아웃 컴포넌트 완료

---
+++ leave_message/src/components/Out.js
@@ -0,0 +1,12 @@
+import "../styles/components/Out.css";
+
+const Out = () => {
+    return(
+        <div className="Out">
+            <div className="Out-logout-btn">로그아웃</div>
+            <div className="Out-withdrawal-btn">회원탈퇴</div>
+        </div>
+    );
+}
+
+export default Out;
